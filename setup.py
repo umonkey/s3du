f551ade0328fb6db96da0deb9291715e8bef02b1
Justin Forest <hex@umonkey.net>
2019-10-12 16:57:15 +0300

    Version 0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ most storage.
 
 setup(
     name='s3du',
-    version='0.4',
+    version='0.5',
     description='Inspect Amazon S3 buckets interactively with ncdu',
     author='Justin Forest',
     author_email='hex@umonkey.net',
